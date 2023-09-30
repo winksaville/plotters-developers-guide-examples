479fa796b82cd62b124396aba3043db14f7cdd3e
Wink Saville <wink@saville.com>
2023-09-30 11:47:08 -0700

    feat: Add draw_figure_components
    Add adding mesh, axes and titles.

--- examples/all.rs
+++ examples/all.rs
@@ -1,12 +1,16 @@
 mod getting_started;
 mod define_chart_context;
+mod draw_figure_components;
 
 use getting_started::sin_wave;
 use define_chart_context::{chart_builder, draw_series};
-
+use draw_figure_components::{configure_mesh, configure_axes, configure_title};
 
 fn main() {
     sin_wave();
     chart_builder();
     draw_series();
+    configure_mesh();
+    configure_axes();
+    configure_title();
 }
